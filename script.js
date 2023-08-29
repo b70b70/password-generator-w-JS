7a91dd70b30ad769b743325581a291f8fd9b67f3
Ali <134099889+b70b70@users.noreply.github.com>
2023-08-29 20:56:22 +0100

    created password generator. user will be prompted to choose password length between 10 - 64. alerts added for failsafes on character lengths. Also gives user option to select type of password with confirm prompt (i.e. numbers & characters. failsafe added to ensure atleast one type of password is selected. then generate password function has been created in-line with user's preferences.

--- script.js
+++ script.js
@@ -88,23 +88,112 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
+// Function to prompt user for password options - will be a variable to store length of password from user input
 function getPasswordOptions() {
+  let lengthForPw = parseInt(
+    prompt("How many characters would you like your password to contain?")
+  )
 
+  // Checks if number provided in prompt is a number
+  if (isNaN(lengthForPw) === true) { 
+    alert(`Password length must be provided as a number`); 
+    return;
+  }
+
+   // alerts user on character length limit 
+  if(lengthForPw < 10) {
+    alert ('Password length must be atleast 10 characters please');
+    return;
+  }
+
+  if(lengthForPw > 64) {
+    alert ('Password length must be less than 65 characters please');
+    return;
+  }
+
+  // Confirm character type 
+  let hasSpecialChars = confirm(`Click ok to confirm including special characters`);
+  let hasNumbers = confirm(`Click ok to confirm including numerical characters`);
+  let hasLowerCaseChars = confirm(`Click ok to confirm including lower case characters`);
+  let hasUpperCaseChars = confirm(`click ok to confirm including upper case characters`);
+  
+  // Check if one of the character types is selected
+  if (hasSpecialChars === false &&
+    hasNumbers === false &&
+    hasLowerCaseChars === false &&
+    hasUpperCaseChars === false) {
+    alert("You must select at least one character type.");
+    return;
+  }
+
+  // here i return an object which contains the users options
+  let passwordOptions = {
+    length: lengthForPw,
+    hasSpecialChars: hasSpecialChars,
+    hasLowerCaseChars: hasLowerCaseChars,
+    hasUpperCaseChars: hasUpperCaseChars,
+    hasNumbers: hasNumbers
+  }
+
+  return passwordOptions;
+
+  
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  let randomElement = arr[randomIndex];
 
+  return randomElement;
+  
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  let options = getPasswordOptions();
 
+  let result = []
+  let possibleCharacters = []
+  let guaranteedCharacters = []
+
+/*
+  The concat() method concatenates (joins) two or more arrays.
+*/
+if (options.hasSpecialChars) { 
+  possibleCharacters = possibleCharacters.concat(specialCharacters);
+  guaranteedCharacters.push(getRandom(specialCharacters));
+}
+
+if (options.hasLowerCaseChars) {
+  possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+  guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+}
+
+if (options.hasNumbers) {
+  possibleCharacters = possibleCharacters.concat(numericCharacters);
+  guaranteedCharacters.push(getRandom(numericCharacters));
+}
+
+if (options.hasUpperCaseChars) {
+  possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+  guaranteedCharacters.push(getRandom(upperCasedCharacters));
+}
+  
+for (let index = guaranteedCharacters.length; index < options.length; index++) { 
+  let generated = getRandom(possibleCharacters);
+  result.push(generated);
+}
+
+result = result.concat(guaranteedCharacters);
+
+return result.join("");
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
+
+
 
 // Write password to the #password input
 function writePassword() {
